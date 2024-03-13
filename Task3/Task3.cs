e092a8a03cef327b64c689d5657844311f7487b9
Evgeniy K <kovalev8915512@gmail.com>
2024-03-13 20:24:07 +0300

    Task3 complite

---
+++ Task3/Task3.cs
@@ -0,0 +1,24 @@
+﻿// Задайте произвольный массив. Выведете его
+// элементы, начиная с конца используя рекурсию.
+
+
+  int [] arr= {3,7,9,10,15,20};
+  ShowArray(arr);
+  
+  
+static void ShowArray(int [] array){
+    
+    if (array.Length==0){
+        return;
+    }
+    else{
+        int[] newArray = new int[array.Length - 1];
+    for (int i = 0; i < newArray.Length; i++){
+        newArray[i]=array[i];
+    }
+        Console.WriteLine(array[array.Length-1]);
+        ShowArray(newArray);
+        
+    }
+    
+}
