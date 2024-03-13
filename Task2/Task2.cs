d2d17f6dd1f58011e094bc8fe5385ab2c1c707ff
Evgeniy K <kovalev8915512@gmail.com>
2024-03-13 20:18:56 +0300

    Task2 complite

---
+++ Task2/Task2.cs
@@ -0,0 +1,24 @@
+﻿
+// Задача 2:Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+Console.WriteLine(Akker(3,2));	
+	
+	
+
+
+static int Akker(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return Akker(m - 1, 1);
+			}
+			return Akker(m - 1, Akker(m, n - 1));
+		}
+
+	
+		
